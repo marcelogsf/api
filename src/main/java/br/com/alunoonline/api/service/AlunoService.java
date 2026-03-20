fb3c2ad48b0fa91ac940e596766850bf0cc6f771
marcelofilho <marcelofilho2310@gmail.com>
2026-03-19 22:22:41 -0300

    .

--- src/main/java/br/com/alunoonline/api/service/AlunoService.java
+++ src/main/java/br/com/alunoonline/api/service/AlunoService.java
@@ -1,4 +1,49 @@
 package br.com.alunoonline.api.service;
 
+import br.com.alunoonline.api.model.Aluno;
+import br.com.alunoonline.api.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.List;
+import java.util.Optional;
+
+@Service  // "Esta classe é um Service do Spring!"
 public class AlunoService {
+
+    @Autowired  // Injeção de dependência automática!
+    AlunoRepository alunoRepository;
+
+    // CREATE — salvar novo aluno
+    public void criarAluno(Aluno aluno) {
+        alunoRepository.save(aluno);  // INSERT INTO aluno ...
+    }
+
+    // READ — listar todos
+    public List<Aluno> listarTodosAlunos() {
+        return alunoRepository.findAll();  // SELECT * FROM aluno
+    }
+
+    // READ — buscar por ID
+    public Optional<Aluno> buscarAlunoPorId(Long id) {
+        return alunoRepository.findById(id);  // SELECT * FROM aluno WHERE id = ?
+    }
+
+    // UPDATE — atualizar por ID
+    public void atualizarAlunoPorId(Long id, Aluno aluno) {
+        Optional<Aluno> alunoDoBanco = buscarAlunoPorId(id);
+
+        if (alunoDoBanco.isEmpty()) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Aluno não encontrado");
+        }
+
+        Aluno alunoParaEditar = alunoDoBanco.get();
+        alunoParaEditar.setNome(aluno.getNome());
+        alunoParaEditar.setCpf(aluno.getCpf());
+        alunoParaEditar.setEmail(aluno.getEmail());
+
+        alunoRepository.save(alunoParaEditar);  // UPDATE aluno SET ...
+    }
 }
